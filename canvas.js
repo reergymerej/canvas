12e61765b2eba99b99419bcb4cccc97172b99686
Jeremy Greer <jeremygreer@reergymerej.com>
2014-06-27 17:00:44 -0400

    add collision detection

--- canvas.js
+++ canvas.js
@@ -3,6 +3,7 @@ var c = (function () {
     var ctx;
     var width;
     var height;
+    var actors = [];
 
     /**
     * @name setCanvas
@@ -37,6 +38,10 @@ var c = (function () {
     */
     var animate = function (animationFn) {
         clear();
+        actors.forEach(function (actor) {
+            actor.animate();
+        });
+
         if (animationFn() !== false) {
             setTimeout(function () {
                 requestAnimationFrame(function () {
@@ -49,17 +54,27 @@ var c = (function () {
     /**
     * @name circle
     * @description draw a circle
-    * @param {Number} x
-    * @param {Number} y
+    * @param {Number} config.x
+    * @param {Number} config.y
+    * @param {Number} config.radius
+    * @param {String} config.color
     */
-    var circle = function (x, y, radius) {
-        ctx = canvas.getContext('2d');
+    var circle = function (config) {
+        var x, y, r, c;
+
         if (ctx) {
-            y = y + 0.5 * radius;
-            x = x + 0.5 * radius;
+            x = config.x;
+            y = config.y;
+            r = config.radius;
+            c = config.color || '#000';
+
+            y = y + r / 2;
+            x = x + r / 2;
+
             ctx.beginPath();
             ctx.moveTo(x, y);
-            ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+            ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+            ctx.fillStyle = c;
             ctx.fill();
         }
     };
@@ -78,12 +93,15 @@ var c = (function () {
     Actor.prototype.position = function (x, y) {
         this.x = x;
         this.y = y;
+        this.w = 0;
+        this.h = 0;
     };
 
     Actor.prototype.onFrame = function () {};
 
     Actor.prototype.draw = function () {
         console.log('define draw method');
+        // set the w & h when you're done
     };
 
     Actor.prototype.move = function () {
@@ -94,7 +112,40 @@ var c = (function () {
     Actor.prototype.animate = function () {
         this.onFrame();
         this.move();
+        if (this.checkForCollisions()) {
+            this.onCollision();
+        }
         this.draw();
+    };
+
+    Actor.prototype.checkForCollisions = function () {
+        var that = this,
+            collidesWith = [];
+
+        actors.forEach(function (actor) {
+
+            if (actor !== that) {
+                if (that.intersect(actor)) {
+                    collidesWith.push(actor);
+                }
+            }
+        });
+
+        this.collidesWith = collidesWith;
+        return collidesWith.length > 0;
+    };
+
+    Actor.prototype.intersect = function (actor) {
+        var intersect = false,
+            xDist = Math.abs(actor.x - this.x),
+            yDist = Math.abs(actor.y - this.y);
+
+        return xDist < actor.w && xDist < this.w &&
+            yDist < actor.h && yDist < this.h;
+    };
+
+    Actor.prototype.onCollision = function () {
+        console.log('implement me');
     };
 
     Actor.prototype.constrain = function () {
@@ -117,6 +168,21 @@ var c = (function () {
         return height;
     };
 
+    var addActor = function (actor) {
+        actors.push(actor);
+    };
+
+    var rand = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    var getRandomColor = function (alpha) {
+        var r = rand(0, 255),
+            g = rand(0, 255),
+            b = rand(0, 255);
+        return 'rgba(' + r + ',' + g + ',' + b + ',' + alpha + ')';
+    };
+
     return {
         canvas: setCanvas,
         clear: clear,
@@ -124,6 +190,9 @@ var c = (function () {
         animate: animate,
         Actor: Actor,
         w: w,
-        h: h
+        h: h,
+        addActor: addActor,
+        rand: rand,
+        getRandomColor: getRandomColor
     };
 }());
--- index.js
+++ index.js
@@ -13,7 +13,14 @@ $(function () {
     Circle.prototype = new c.Actor();
 
     Circle.prototype.draw = function () {
-        c.circle(this.x, this.y, this.radius);
+        c.circle({
+            x: this.x,
+            y: this.y,
+            radius: this.radius,
+            color: this.color
+        });
+        this.w = this.radius * 2;
+        this.h = this.radius * 2;
     };
 
     Circle.prototype.onFrame = function () {
@@ -46,24 +53,54 @@ $(function () {
         this.constructor.prototype.onFrame();
     };
 
-    var circle = new Circle(0, 100, 10);
-    circle.constrain();
-    circle.xSpeed = 12;
-    circle.ySpeed = 7;
+    Circle.prototype.onCollision = function () {
+        this.xSpeed *= 0.8; 
+        // this.ySpeed *= -1;
+    };
 
-    var circle2 = new Circle(90, 88, 14);
-    circle2.constrain();
-    circle2.xSpeed = 6;
-    circle2.ySpeed = 5;
+    var getABunchOfBalls = function () {
+        var i,
+            max = 40,
+            balls = [],
+            circle,
+            maxSpeed = 30;
+
+        for (i = 0; i < max; i++) {
+            circle = new Circle(i * 10, i * 10, 6);
+            circle.xSpeed = Math.min((i + 1) / 10, maxSpeed);
+            circle.ySpeed = Math.min((i + 1) / 10, maxSpeed);
+            circle.color = c.getRandomColor(0.5);
+
+            balls.push(circle);
+        }
+
+        return balls;
+    };
 
     c.canvas('canvas');
+    
+    // var b1 = new Circle(100, 100, 12);
+    // b1.xSpeed = 1;
+    // b1.ySpeed = 0;
+    // b1.color = c.getRandomColor(0.5);
+    // c.addActor(b1);
+
+    // var b2 = new Circle(200, 100, 12);
+    // b2.xSpeed = -1;
+    // b2.ySpeed = 0.3;
+    // b2.color = c.getRandomColor(0.5);
+    // c.addActor(b2);
+
+    getABunchOfBalls().forEach(function (ball) {
+        c.addActor(ball);
+    });
 
     $('#circle').on('click', function () {
-        c.animate(function () {
-            circle.animate();
-            circle2.animate();
+        var start = Date.now(),
+            runTime = 60;
 
-            // return circle.x > 400;
+        c.animate(function () {
+            return Date.now() - start < runTime * 1000;
         });
     });
 
